c2311c98f60b5f254222f0a0f6ba364397883198
Uma Anitha Chitra <umaanithachitra@gmail.com>
2026-07-22 10:57:22 +0530

    Add files via upload

---
+++ largest of two numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main(){
+	int a,b;
+	cout<<"enter two numbers:"<<endl;
+	cin>>a>>b;
+	if(a>b)
+		cout<<"large numbers="<<a;
+	else
+		cout<<"large number="<<b;
+}
