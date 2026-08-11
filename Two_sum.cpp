e4a5e8ecfe134faf71e1481322ae28c7c0ba1459
Uma Anitha Chitra <umaanithachitra@gmail.com>
2026-08-11 11:01:58 +0530

    Add files via upload

---
+++ Two_sum.cpp
@@ -0,0 +1,22 @@
+//two sum
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+	int sum;
+	cin>>sum;
+	for(int i=0;i<n;i++){
+		for(int j=i+1;j<n;j++){
+			if(a[i]+a[j]==sum){
+				cout<<a[i]<<" "<<a[j]<<endl;
+				break;
+			}
+		}
+		
+	}
+}
